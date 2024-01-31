ec1bb0d387c1706662e9c8f5066ef05301c877cf
caio-d <caio__leo@hotmail.com>
2024-01-31 05:46:50 -0300

    chegando perto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContaComponent } from './pages/conta/conta.component';
+import { CarteiraComponent } from './components/carteira/carteira.component';
 
-const routes: Routes = [{}];
+const routes: Routes = [
+  { path: '', component: ContaComponent },
+  { path: 'carteira', component: CarteiraComponent },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
-
 import { AppComponent } from './app.component';
 import { CarteiraComponent } from './components/carteira/carteira.component';
 
